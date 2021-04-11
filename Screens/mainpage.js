ded2b09838894f0783e4f0f0561b007c1d38e45d
janhavi425 <jachwale@gmail.com>
2021-04-11 21:58:55 +0530

    Second Commit

--- Screens/mainpage.js
+++ Screens/mainpage.js
@@ -3,47 +3,65 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import * as Animatable from "react-native-animatable";
 
-const mainpage = ({navigation}) => {
+const mainpage = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text
-          style={{
-            fontWeight: "900",
-            fontSize: 40,
-            fontStyle: "italic",
-            paddingLeft: "10%",
-            paddingTop: "20%",
-          }}
-        >
-          Daily
-        </Text>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 40,
-            fontStyle: "italic",
-            paddingLeft: "10%",
-          }}
-        >
-          Weather
-        </Text>
+      <View
+        style={{ flex: 1, justifyContent: "center", alignSelf: "flex-end",flexDirection:'row' }}
+      >
+        <Image
+            style={{
+              height: "110%",
+              width: "52%",
+              alignSelf:"flex-start",
+              //marginLeft: "50%",
+              marginTop:70,
+              //paddingRight:20
+            }}
+            source={require("../assets/jj1.png")}
+          />
+        <View style={{flexDirection:'column'}}>
+          <Text
+            style={{
+              fontWeight: "900",
+              fontSize: 40,
+              fontStyle: "italic",
+              paddingLeft: "10%",
+              paddingTop: "20%",
+            }}
+          >
+            Daily
+          </Text>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 40,
+              fontStyle: "italic",
+              paddingRight: "10%",
+              left: 0,
+            }}
+          >
+            Weather
+          </Text>
+        </View>
       </View>
       <View style={{ flex: 4, justifyContent: "center" }}>
-        <View
-          style={{ flex: 4, justifyContent: "center",marginTop:"10%" }}
-        >
+        <View style={{ flex: 4, justifyContent: "center", marginTop: "25%" }}>
           <Image
             style={{
-              height: "90%",
-              width: "70%",
+              height: "150%",
+              width: "110%",
               alignSelf: "flex-end",
-              marginRight: "10%",
+              marginRight: "-8%",
+
             }}
-            source={require("../assets/p9.png")}
+            source={require("../assets/P2.png")}
           />
         </View>
-        <Animatable.View animation="fadeInUp" style={{ flex: 1, justifyContent: "center" }}>
+        <Animatable.View
+          animation="fadeInUp"
+          style={{ flex: 1, justifyContent: "center" }}
+        >
           <TouchableOpacity
             style={{
               backgroundColor: "#37003c",
@@ -53,10 +71,7 @@ const mainpage = ({navigation}) => {
               alignSelf: "center",
               justifyContent: "center",
             }}
-            onPress={() =>
-              navigation.navigate("HomeScreen"
-              )
-            }
+            onPress={() => navigation.navigate("HomeScreen")}
           >
             <Text style={{ alignSelf: "center", color: "white", fontSize: 30 }}>
               Let's start
